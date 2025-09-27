3830e0209c3bed762dd6e201e29f5b68c9b9978c
8Air28 <koyhoid32tihhsni@gmail.com>
2025-09-28 00:46:19 +0900

    Add files via upload

--- proxy_server.py
+++ proxy_server.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__, static_folder="client")
 
 # Hugging Face APIキー
-HF_API_KEY = "YOUR_Hhf_IkWiBdgSqRkVOyPlhIaBEHPKFlhERqZXoCUGGINGFACE_API_KEY"
+HF_API_KEY = "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"
 
 HF_API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
 
